1bf83f72771d4e964f429f34ca1e0ad5c41655e9
Colton <allen.colton@hotmail.com>
2023-02-27 18:41:04 -0800

    created api route to handle form submission

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  
+</script>
+
+<h1>Pokémon Battle Strategist</h1>
+<form>
+  <label for="game">Select game: </label>
+  <select name="game" id="game">
+    <option value="Pokémon Blue">Pokémon Blue</option>
+    <option value="Pokémon Red">Pokémon Red</option>
+    <option value="Pokémon Yellow">Pokémon Yellow</option>
+    <option value="Pokémon Silver">Pokémon Silver</option>
+    <option value="Pokémon Gold">Pokémon Gold</option>
+  </select>
+  <div class="field-group">
+    <label for="opponent">Opponent: </label>
+    <input type="text" name="opponent" id="opponent">
+  </div>
+</form>
---
+++ src/routes/api/strat/+server.ts
@@ -0,0 +1,71 @@
+import { OPENAI_SECRET_KEY } from "$env/static/private"
+import { oneLine, stripIndent } from "common-tags"
+import type { RequestHandler } from "./$types"
+import type { CreateCompletionRequest } from 'openai'
+import { error } from "@sveltejs/kit"
+
+export const POST: RequestHandler = async ({ request }) => {
+
+  try {
+    if (!OPENAI_SECRET_KEY) {
+      throw new Error('OPENAI_SECRET_KEY not set')
+    }
+
+    const requestData = await request.json()
+
+    if (!requestData) {
+      throw new Error('Request data missing...')
+    }
+
+    const { game, opponent } = requestData
+
+    if (!game) {
+      throw new Error("No game provided...");
+    }
+
+    const prompt = stripIndent`
+      ${oneLine`
+        You are a professional Pokemon Trainer who also trains other pokemon trainers to become the best there ever was. Please provide a list of 6 pokemon that would be most effective against the given Gym Leader in the provided game listed below.
+      `}
+
+      Pokemon Game: """${game.trim()}"""
+      Gym Leader: """${opponent.trim()}"""
+
+      Answer:
+    `
+
+    const completionOpts: CreateCompletionRequest = {
+      model: 'text-davinci-003',
+      prompt,
+      max_tokens: 256,
+      temperature: 0.9,
+      stream: true
+    }
+
+    const response = await fetch('https://api.openai.com/v1/completions', {
+      headers: {
+        Authorization: `Bearer ${OPENAI_SECRET_KEY}`,
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify(completionOpts)
+    })
+
+    if (!response.ok) {
+      const err = await response.json()
+      console.log(err)
+      throw new Error("Failed to create completion", err);
+    }
+
+    return new Response(response.body, {
+      headers: {
+        'Content-Type': 'text/event-stream'
+      }
+    })
+
+  } catch (err) {
+    console.error(err)
+    throw error(500, 'An error occured...')
+  }
+
+}
